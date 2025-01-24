5792113a990deccfbdb562ce4bea2b5d6ea46a3b
Haspinder Kaur <kauhaspi@sheridancollege.ca>
2025-01-24 11:23:40 -0500

    first_code

---
+++ src/friday03_2025/Friday03_2025.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package friday03_2025;
+
+/**
+ *
+ * @author haspinderkaur
+ */
+public class Friday03_2025 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hey, How are you? ");
+    }
+    
+}
